d746ebb45eb2f3f2c94ab0e667bf1906d9e0708f
vzvenigorosky <123944766+vzvenigorosky@users.noreply.github.com>
2024-03-23 00:09:33 +0100

    Create quizData.js

---
+++ quizData.js
@@ -0,0 +1,48 @@
+// quiz-data.js
+const quizData = [
+    {
+        question: "Qu'est-ce que l'ADN mitochondrial ?",
+        correctAnswer: "ADN présent dans les mitochondries, hérité de la mère",
+        wrongAnswers: [
+            "ADN présent dans le noyau des cellules",
+            "ADN présent dans les chloroplastes",
+            "ADN hérité du père",
+            "ADN utilisé pour la photosynthèse",
+            "ADN impliqué dans la respiration cellulaire",
+            "ADN trouvé uniquement chez les animaux",
+            "ADN qui détermine le type de sang",
+            "ADN qui code pour les protéines virales"
+        ]
+    },
+
+  {
+    question: "Quelle est la fonction principale de l'ADN ?",
+    correctAnswer: "Stockage de l'information génétique",
+    wrongAnswers: [
+        "Production d'énergie cellulaire",
+        "Transport de l'oxygène dans le sang",
+        "Digestion des aliments",
+        "Régulation de la température corporelle",
+        "Transmission des signaux nerveux",
+        "Protection contre les maladies",
+        "Absorption des nutriments",
+        "Élimination des déchets corporels"
+    ]
+},
+{
+    question: "Qu'est-ce qu'une cellule ?",
+    correctAnswer: "L'unité de base de la vie",
+    wrongAnswers: [
+        "Un type de molécule organique",
+        "Une structure trouvée uniquement dans les plantes",
+        "Un composant du sang qui transporte l'oxygène",
+        "Une partie du cerveau responsable de la mémoire",
+        "Une entité qui cause des maladies",
+        "Un outil utilisé en biotechnologie",
+        "Un terme pour décrire une réaction chimique",
+        "Le processus par lequel l'énergie solaire est convertie en énergie chimique"
+    ]
+}
+
+    // Add more questions with their correct and wrong answers
+];
